fa1d09e34bb14896a673dc4e17fde60235a37478
Kaue-G <kaueguedes.dev@gmail.com>
2021-09-10 18:01:16 -0300

    Paginas de Historico e Cancelar Ticket , semi ok.

--- react-frontend/src/pages/CancelPage/index.jsx
+++ react-frontend/src/pages/CancelPage/index.jsx
@@ -1,11 +1,35 @@
+import './style.css';
 import { getOffice } from '../../services/offices';
+import Aside from '../../components/Aside';
 
 function CancelPage(props) {
   const officeId = props.match.params.officeId;
   const office = getOffice(officeId);
 
   return (
-    <h1>Cancelamento {office.name}</h1>
+    // <h1>Cancelamento {office.name}</h1>
+    <div className="container">  
+      <div className="page">
+        <Aside />
+        <div className="cancelarTicket">
+          <div className ="txt">
+            <h1>cancelar ticket</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Eveniet officiis veniam facere eum vitae soluta perferendis 
+              reprehenderit ipsa unde! Quaerat doloribus atque id, nisi 
+              quibusdam accusamus quod placeat inventore beatae.
+            </p>
+          </div>
+          <div>
+            <form className="form" action="">
+              <input type="email" name="" id="" />
+              <button>buscar pelo e-mail</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- react-frontend/src/pages/HistoricPage/index.jsx
+++ react-frontend/src/pages/HistoricPage/index.jsx
@@ -1,14 +1,37 @@
+import Aside from '../../components/Aside';
 import './style.css';
 
 import Disponibility from "../../components/Disponibility";
 
 function Home() {
   return (
-    <>
-      <main>
-        <Disponibility />
-      </main>
-    </>
+    // <>
+    //   <main>
+    //     <Disponibility />
+    //   </main>
+    // </>
+    <div className="container">  
+      <div className="page">
+        <Aside />
+        <div className="cancelarTicket">
+          <div className ="txt">
+            <h1>historico</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Eveniet officiis veniam facere eum vitae soluta perferendis 
+              reprehenderit ipsa unde! Quaerat doloribus atque id, nisi 
+              quibusdam accusamus quod placeat inventore beatae.
+            </p>
+          </div>
+          <div>
+            <form className="form" action="">
+              <input type="email" name="" id="" />
+              <button>buscar pelo e-mail</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
