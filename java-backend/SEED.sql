609380250048285e1b3af585c8cb8c47418b7181
NihwlCat <pedro.ra.nihwl@gmail.com>
2021-09-14 20:59:10 -0300

    Dockerfile and jar build last version

--- java-backend/SEED.sql
+++ java-backend/SEED.sql
@@ -72,14 +72,15 @@ CREATE TABLE Disponibility_Room(
 );
 
 -- Escritórios
-INSERT INTO Office(address,capacity,name) VALUES ('Rua Bela Cintra, 986 - 2º andar, Consolação, São Paulo - SP',21,'São Paulo');
-INSERT INTO Office(address,capacity,name) VALUES ('Praça Dos Expedicionários, 192º andar, Gonzaga, Santos - SP',10,'Santos');
+INSERT INTO Office(address,capacity,name) VALUES ('Rua Bela Cintra, 986 - 2º andar, Consolação, São Paulo - SP',25,'São Paulo');
+INSERT INTO Office(address,capacity,name) VALUES ('Praça Dos Expedicionários, 192º andar, Gonzaga, Santos - SP',25,'Santos');
 
 SELECT * FROM Office;
 
 -- Cadeiras
-INSERT INTO Room(id,capacity, name, office_id) VALUES (1,10,'Sala 1',3);
+INSERT INTO Room(id,capacity, name, office_id) VALUES (1,4,'Sala 1',3);
 INSERT INTO Room(id,capacity, name, office_id) VALUES (2,4,'Sala 2',3);
+INSERT INTO Room(id,capacity, name, office_id) VALUES (5,4,'Sala 5',3);
 INSERT INTO Room(id,capacity, name, office_id) VALUES (35,4,'Sala 35',4);
 INSERT INTO Room(id,capacity, name, office_id) VALUES (34,4,'Sala 34',4);
 
@@ -169,3 +170,31 @@ INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('20
 INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',16,4,true);
 INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',17,4,true);
 INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',18,4,true);
+
+
+-- Terça - Office 2
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',8,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',9,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',10,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',11,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',12,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',13,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',14,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',15,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',16,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',17,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',18,4,true);
+
+
+-- Quarta - Office 2
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',8,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',9,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',10,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',11,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',12,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',13,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',14,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',15,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',16,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',17,4,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',18,4,true);
--- java-backend/src/main/resources/import.sql
+++ java-backend/src/main/resources/import.sql
@@ -1,12 +1,13 @@
 -- Escritórios
-INSERT INTO Office(address,capacity,name) VALUES ('Rua Bela Cintra, 986 - 2º andar, Consolação, São Paulo - SP',25,'São Paulo'); -- 10
-INSERT INTO Office(address,capacity,name) VALUES ('Praça Dos Expedicionários, 192º andar, Gonzaga, Santos - SP',25,'Santos'); -- 10
+INSERT INTO Office(address,capacity,name) VALUES ('Rua Bela Cintra, 986 - 2º andar, Consolação, São Paulo - SP',25,'São Paulo');
+INSERT INTO Office(address,capacity,name) VALUES ('Praça Dos Expedicionários, 192º andar, Gonzaga, Santos - SP',25,'Santos');
 
 -- Cadeiras
 INSERT INTO Room(id,capacity, name, office_id) VALUES (1,4,'Sala 1',1);
-INSERT INTO Room(id,capacity, name, office_id) VALUES (2,6,'Sala 2',1);
+INSERT INTO Room(id,capacity, name, office_id) VALUES (2,4,'Sala 2',1);
+INSERT INTO Room(id,capacity, name, office_id) VALUES (5,4,'Sala 5',1);
 INSERT INTO Room(id,capacity, name, office_id) VALUES (35,4,'Sala 35',2);
-INSERT INTO Room(id,capacity, name, office_id) VALUES (34,6,'Sala 34',2);
+INSERT INTO Room(id,capacity, name, office_id) VALUES (34,4,'Sala 34',2);
 
 -- Funcionários
 INSERT INTO Employee(cpf, business_unit, email) VALUES ('111','DEV','ana@gmail.com');
@@ -14,96 +15,109 @@ INSERT INTO Employee(cpf, business_unit, email) VALUES ('222','DEV','carlos@gmai
 INSERT INTO Employee(cpf, business_unit, email) VALUES ('333','SCRUM','miguel@gmail.com');
 INSERT INTO Employee(cpf, business_unit, email) VALUES ('444','UX','junior@gmail.com');
 INSERT INTO Employee(cpf, business_unit, email) VALUES ('555','UX','vanessa@gmail.com');
+INSERT INTO Employee(cpf, business_unit, email) VALUES ('666','DEV','guilherme@gmail.com');
 
--- Horários livres (06/09/2021 - 11/09/2021)
+-- Disponibilidades de 13/09 a 17/09
 -- Segunda
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-06',8,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-06',9,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-06',10,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-06',11,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-06',12,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-06',13,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-06',14,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-06',15,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-06',16,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-06',17,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-06',18,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',8,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',9,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',10,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',11,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',12,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',13,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',14,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',15,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',16,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',17,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',18,1,true);
 
 -- Terça
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-07',8,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-07',9,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-07',10,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-07',11,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-07',12,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-07',13,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-07',14,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-07',15,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-07',16,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-07',17,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-07',18,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',8,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',9,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',10,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',11,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',12,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',13,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',14,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',15,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',16,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',17,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',18,1,true);
 
 -- Quarta
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',8,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',9,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',10,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',11,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',12,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',13,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',14,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',15,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',16,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',17,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',18,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',8,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',9,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',10,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',11,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',12,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',13,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',14,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',15,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',16,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',17,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',18,1,true);
 
 -- Quinta
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-09',8,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-09',9,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-09',10,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-09',11,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-09',12,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-09',13,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-09',14,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-09',15,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-09',16,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-09',17,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-09',18,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-16',8,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-16',9,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-16',10,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-16',11,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-16',12,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-16',13,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-16',14,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-16',15,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-16',16,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-16',17,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-16',18,1,true);
 
 -- Sexta
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-10',8,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-10',9,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-10',10,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-10',11,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-10',12,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-10',13,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-10',14,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-10',15,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-10',16,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-10',17,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-10',18,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-17',8,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-17',9,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-17',10,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-17',11,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-17',12,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-17',13,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-17',14,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-17',15,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-17',16,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-17',17,1,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-17',18,1,true);
 
--- Sábado
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-11',8,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-11',9,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-11',10,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-11',11,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-11',12,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-11',13,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-11',14,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-11',15,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-11',16,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-11',17,1,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-11',18,1,true);
+-- Segunda do Escritório 2
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',8,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',9,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',10,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',11,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',12,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',13,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',14,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',15,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',16,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',17,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-13',18,2,true);
 
--- Office 2 na quarta
+-- Terça - Office 2
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',8,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',9,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',10,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',11,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',12,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',13,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',14,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',15,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',16,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',17,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-14',18,2,true);
 
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',8,2,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',9,2,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',10,2,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',11,2,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',12,2,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',13,2,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',14,2,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',15,2,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',16,2,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',17,2,true);
-INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-08',18,2,true);
+-- Quarta - Office 2
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',8,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',9,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',10,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',11,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',12,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',13,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',14,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',15,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',16,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',17,2,true);
+INSERT INTO Disponibility(date_pk, hour_pk, office_pk, is_available) VALUES ('2021-09-15',18,2,true);
